7187bae76244c8fa31ed12a3dc65722cf17c9d7a
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-12-12 10:17:20 +0100

    Devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171210201339) do
+ActiveRecord::Schema.define(version: 20171212090612) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "vorname"
@@ -21,9 +21,22 @@ ActiveRecord::Schema.define(version: 20171210201339) do
     t.integer  "fahrerbewertung"
     t.integer  "mitfahrerbewertung"
     t.string   "standort"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "accounts", ["email"], name: "index_accounts_on_email", unique: true
+  add_index "accounts", ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
 
   create_table "fahrts", force: :cascade do |t|
     t.string   "von"
