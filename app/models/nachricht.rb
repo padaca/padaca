36e00b055ba33f703052d61552b91bb88fbf8979
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2017-12-21 13:19:30 +0100

    validate messages

--- app/models/nachricht.rb
+++ app/models/nachricht.rb
@@ -1,4 +1,6 @@
 class Nachricht < ActiveRecord::Base
   belongs_to :sender, class_name: "Account"
   belongs_to :empfaenger, class_name: "Account"
+  validates :sender, presence: true
+  validates :empfaenger, presence: true
 end
