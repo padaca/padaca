5649d7f05ad99554549240db652b9e23e0b65f3e
Jonas <jonasrieskamp@googlemail.com>
2018-01-04 10:38:06 +0100

    Deaktivierte Accounts können sich nicht mehr anmelden und das Impressum ist auch erreichbar, wenn man nicht angemeldet ist

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,16 +4,29 @@ class Account < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # to String
   def to_s
     "#{self.email}"
   end
 
+  # activates an account
   def activate_account!   
     update_attribute :is_active, true 
   end
   
+  # deactivates an account
   def deactivate_account!   
     update_attribute :is_active, false 
+  end
+
+  # returns whether the account ist active
+  def active_for_authentication?
+    super and self.is_active?
+  end
+
+  # returns a message when account is deactive
+  def inactive_message
+    "You are not allowed to log in."
   end
 
 end
