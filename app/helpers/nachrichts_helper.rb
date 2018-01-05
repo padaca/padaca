bf8071db9e63e45830516741c3ebb43c0f3587a8
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2018-01-05 22:03:30 +0100

    unread messages indicator in menu

--- app/helpers/nachrichts_helper.rb
+++ app/helpers/nachrichts_helper.rb
@@ -1,2 +1,9 @@
 module NachrichtsHelper
+
+    def unread_nachrichts_class
+        return unless current_account
+        if Nachricht.where(empfaenger_id: current_account.id, read: nil).count > 0
+            :unread
+        end
+    end
 end
