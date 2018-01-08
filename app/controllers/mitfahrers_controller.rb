58085912a5e00445534bc45a268049812dd4e4e1
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2018-01-08 14:43:27 +0100

    Mitfahrers

--- app/controllers/mitfahrers_controller.rb
+++ app/controllers/mitfahrers_controller.rb
@@ -4,20 +4,13 @@ class MitfahrersController < ApplicationController
   # GET /mitfahrers
   # GET /mitfahrers.json
   def index
-    # Liste der Fahrten und Mitfahten des aktuellen Users
-    # select * from fahrts f left join mitfahrers m on f.id = m.fahrt_id where f.account_id = 1 or m.account_id = 1;
-    if params[:filter] && !params[:filter].empty?
-      if params[:filter] == '1'
-        # der aktuelle User ist Fahrer
-        @uebersicht = Fahrt.where(account_id: current_account.id)
-      elsif params[:filter] == '0'
-        # der aktuelle User ist Mitfahrer
-        @uebersicht = Fahrt.joins("left join mitfahrers on fahrts.id = mitfahrers.fahrt_id").where("mitfahrers.account_id = #{current_account.id}")
-      end
-    else
-      # alle Fahrten und Mitfahrten
-      @uebersicht = Fahrt.joins("left join mitfahrers on fahrts.id = mitfahrers.fahrt_id").where("fahrts.account_id = #{current_account.id} or mitfahrers.account_id = #{current_account.id}")
+    @counterpart = :mitfahrer
+    @mitfahrers = Mitfahrer.joins(:fahrt).where(fahrts: { account_id: current_account.id })
+
+    if mitfahrer_filter_params[:fahrt]
+      @mitfahrers.where!(fahrt_id: params[:fahrt])
     end
+
   end
 
   # GET /mitfahrers/1
@@ -83,5 +76,9 @@ class MitfahrersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def mitfahrer_params
       params.require(:mitfahrer).permit(:fahrt_id, :account_id, :mitfahrerbewertung, :fahrerbewertung, :istBestatigt)
+    end
+
+    def mitfahrer_filter_params
+      params.permit(:fahrt)
     end
 end
